acd15402de9b30f17d7263b15f38d79e529a7333
Anne-Sophie <anne-sophie.fratzscher@mail.mcgill.ca>
2021-02-11 14:47:02 -0500

    a1 done

--- A1/assignment1.cpp
+++ A1/assignment1.cpp
@@ -1,14 +1,17 @@
-// how to run: g++ -o A1 assignment1.cpp
-			// ./A1
+//COMP322 Assignment 1
+// NAME: Anne-Sophie Fratzscher
+//ID: 260705446
+
+// how to compile: g++ -o a1.out assignment1.cpp
+// how to run: ./a1.out
 
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
 using namespace std;
-// const int rows = 5;
-// const int cols = 5;
-const int rows = 2;
-const int cols = 2;
+
+const int rows = 5;
+const int cols = 5;
 static int runs = 0;
 static int i = 0;
 static int j = 0;
@@ -64,9 +67,11 @@ void checkPassword(){
 	if (!issue){
 		cout<<"Your password has been accepted"<<endl;
 	}
+	cout<<endl;
 }
 
 void convertPhonetic(){
+	//assumes only ONE word as input -> if multiple words, only first word used
 	string input;
 	cout<<"Please enter a word: ";
 	cin>>input;
@@ -155,16 +160,18 @@ void convertPhonetic(){
 		cout<<" ";
 	}
 	cout<<endl;
+	cout<<endl;
 }
 
 void fillMatrix(int matrix[rows][cols]){
 	//initialize rand 
 	//runs too quickly, so need to add something accounting for # times run
+	int n = 1; // TO REMOVE
 	srand(time(NULL) - runs*2);
 	//fill in row 0, then row 1, ...
 	for(int i = 0; i < rows; i++){
 		for(int j = 0; j < cols; j++){
-			//rand() % (max_number + 1 - minimum_number) + minimum_number
+			// HOW TO SET RAND: rand() % (max_number + 1 - minimum_number) + minimum_number
 			matrix[i][j] = (rand() % (25+1-0)+0);
 		}
 	}
@@ -184,25 +191,47 @@ void printMatrix(int matrix[rows][cols]){
 	cout<<endl;
 }
 
-void multiplyMatrices(int matrix[rows][cols],
-						int matrix2[rows][cols],
+void multiplyMatrices(int matrix_left[rows][cols],
+						int matrix_right[rows][cols],
 						int matrix_result[rows][cols]){
-	if (i>cols){
-		return;
+	// set to static so that change values while in function
+	// BUT, declare in function so that remove from memory after function done running
+	static int row1 = 0, col1 = 0, col2 = 0;
+	
+	// if filled all rows of result matrix, are done
+	if (row1 >= rows){ return; }
+
+	// fill column by column 
+	if (col1 < cols){
+		// for each cell, get value
+		if (col2 < cols){
+			matrix_result[row1][col1] += matrix_left[row1][col2] * matrix_right[col2][col1];
+			col2++;
+			multiplyMatrices(matrix_left, matrix_right, matrix_result);
+		}
+		col2=0;
+		col1++;
+		multiplyMatrices(matrix_left, matrix_right, matrix_result);
 	}
-	if (j<)
+
+	// fill result matrix row by row
+	col1=0;
+	row1++;
+	multiplyMatrices(matrix_left, matrix_right, matrix_result);
 }
+
 int main(){
-	// checkPassword();
-	// convertPhonetic();
+	checkPassword();
+	convertPhonetic();
 	int matrix[rows][cols];
 	int matrix2[rows][cols];
-	int matrix_result[rows][cols];
+	int matrix_result[rows][cols] = {0};
 	fillMatrix(matrix);
-	printMatrix(matrix);
 	fillMatrix(matrix2);
+	printMatrix(matrix);
 	printMatrix(matrix2);
 	multiplyMatrices(matrix, matrix2, matrix_result);
-	// printMatrix(matrix_result);
+	printMatrix(matrix_result);
+	return 0;
 
 }
